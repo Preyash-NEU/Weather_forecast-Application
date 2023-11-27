cbf1d3c822df0e9ca75126cd8c0edbd5bacfdc7d
Preyash-NEU <mehta.prey@northeastern.edu>
2023-11-26 19:11:03 -0500

    final changes

---
+++ WeatherApp/src/components/hourlyforecast.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import DayForecast from "./dailyforecast";
+import RainySvg from "./../assets/rainysvg.svg";
+import CloudySvg from "./../assets/cloudysvg.svg";
+import SunnySvg from "./../assets/sunnysvg.svg";
+import WindySvg from "./../assets/windysvg.svg";
+
+const HourlyForecast = ({ data,day }) => {
+  function convertDateFormat(inputString) {
+    const inputDate = new Date(inputString);
+    if (isNaN(inputDate.getTime())) {
+      return "Invalid date";
+    }
+    const options = { day: "numeric", month: "short", year: "numeric" };
+    const formattedDate = inputDate.toLocaleDateString("en-US", options);
+
+    return formattedDate;
+  }
+
+  const typeofWeather = (weather) => {
+    if (weather.includes("rain")) {
+      return RainySvg;
+    } else if (weather.includes("cloud")) {
+      return CloudySvg;
+    } else if (weather.includes("wind")) {
+      return WindySvg;
+    } else if (weather.includes("sun")) {
+      return SunnySvg;
+    } else return SunnySvg;
+  };
+  return (
+    <div>
+      <h2 style={{ color: "black", padding: "1em", margin: "1em" }}>
+        {data.city.name}, {data.city.country}
+      </h2>
+      <div
+        style={{
+          padding: "1em",
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          overflowX:"scroll"
+        }}
+      >
+        {data.list.map((forecast, index) => 
+          {
+            if(forecast.dt_txt.includes( day))return <div
+          
+            key={index}
+            style={{
+              background: "rgba(60, 60, 60,0.5)",
+              margin: "1em",
+              padding: "0em",
+              borderRadius: "20px",
+              color: "black",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "start",
+              width: "18%",
+              minWidth:"250px",
+              
+            }}
+          >
+            <h2>{forecast.dt_txt.split(" ")[1]}</h2>
+            <h4> {forecast.main.temp} K</h4>
+            <p>
+              <img
+                src={typeofWeather(forecast.weather[0].description)}
+                alt={forecast.weather[0].description}
+                style={{ width: "100px", margin: "1em" }}
+              />
+            </p>
+            <h4>Humidity Level: {forecast.main.humidity}%</h4>
+            <h4>Wind Speed: {forecast.wind.speed} m/s</h4>
+          </div>; 
+          else return null
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default HourlyForecast;
